9dce35421d45b59068934277605387e6f79d170f
Ekaterina Novikova <fearnocait@gmail.com>
2026-04-14 15:18:31 +0300

    [feat] add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const STATUS_OPTIONS = ['Alive', 'Dead', 'unknown'];
+export const GENDER_OPTIONS = ['Female', 'Male', 'Genderless', 'unknown'];
+export const SPECIES_OPTIONS = ['Human', 'Alien'];
