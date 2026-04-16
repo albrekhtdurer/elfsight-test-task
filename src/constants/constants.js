887a8c67c9501dfb2f47e3188f7a6a5cb7519262
Ekaterina Novikova <fearnocait@gmail.com>
2026-04-16 10:24:05 +0300

    [fix] sort species

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,16 @@
 export const STATUS_OPTIONS = ['Alive', 'Dead', 'Unknown'];
 export const GENDER_OPTIONS = ['Female', 'Male', 'Genderless', 'Unknown'];
 export const SPECIES_OPTIONS = [
-  'Human',
   'Alien',
-  'Humanoid',
-  'Unknown',
-  'Poopybutthole',
-  'Mythological Creature',
   'Animal',
-  'Robot',
   'Cronenberg',
-  'Disease'
+  'Disease',
+  'Human',
+  'Humanoid',
+  'Mythological Creature',
+  'Poopybutthole',
+  'Robot',
+  'Unknown'
 ];
 
 export const API_URL = 'https://rickandmortyapi.com/api/character/';
